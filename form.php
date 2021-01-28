a25f9417515417db5458bd53d8748ee649ce6ba0
ChloeArd <chloe.ardoise@gmail.com>
2021-01-28 13:19:01 +0100

    Exercice 156

--- form.php
+++ form.php
@@ -4,8 +4,10 @@ $prenom = trim(strip_tags($_POST['username']));
 
     if (!isset($_POST['username'])){
         header("Location: index.php?error=1");
+        echo "Une erreur c'est produite lors du traitement de vos données";
     }
     else{
+        echo "Les données ont bien été envoyés <br>";
         echo "Prénom : $prenom <br>";
     }
 
